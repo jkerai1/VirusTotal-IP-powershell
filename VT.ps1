9db9a8596ae9588bb1d284eae12461824e1e6c40
sgsdgsd <55988027+jkerai1@users.noreply.github.com>
2022-02-17 17:17:25 +0000

    Update VT.ps1

--- VT.ps1
+++ VT.ps1
@@ -3,10 +3,10 @@ param ([Parameter(Mandatory=$true)] $I)  #139.45.197.253
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Function submit-VT($I)
 {
-    $VTbody = @{resource = $I; "X-Apikey" = "KKEY GOES HERE"}
+    $VTbody = @{resource = $I;}
     $headers=@{}
     $headers.Add("Accept", "application/json")
-    $headers.Add("x-apikey", "5b8cf596a8994ff26bf56145e3531dbf6dc845ca0372476b894ae6d50d89fe25")
+    $headers.Add("x-apikey", "KEY GOES HERE")
     $VTResult = Invoke-WebRequest -Uri "https://www.virustotal.com/api/v3/ip_addresses/$I" -Method GET -Headers $headers
     return $VTResult
 }
